89ba405b062edf4336853080fa06d63d1fd1212e
Karadesh <akamalavr@gmail.com>
2021-09-27 04:53:21 +0300

    Todo added_2

---
+++ Todo_project/Todo/models.py
@@ -0,0 +1,24 @@
+from django.utils.timezone import now
+from authnapp.models import ListUser
+from django.db import models
+
+# Create your models here.
+
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    repo_link = models.URLField(max_length=100, default=None)
+    users = models.ForeignKey(ListUser, on_delete=models.CASCADE, default=None)
+
+
+class Todo(models.Model):
+    name = models.OneToOneField(Project, on_delete=models.CASCADE)
+    note_text = models.TextField()
+    creation_date = models.DateField(default=now)
+    update_date = models.DateField(default=now)
+    user = models.ForeignKey(ListUser, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False)
+
+    
+    
---
+++ Todo_project/Todo/serializers.py
@@ -0,0 +1,29 @@
+from django.db.models.fields import CharField
+from rest_framework import serializers
+from Todo.models import Project, Todo
+from authnapp.serializers import ListUserSerializer
+
+
+class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+ #   name = serializers.CharField(max_length=32)
+ #   repo_link = serializers.URLField
+    users = CharField(max_length=32)
+    class Meta:
+        model = Project
+        fields = '__all__'
+    
+    def create(self, validated_data):
+        return Project(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.repo_link = validated_data.get('repo_link', instance.repo_link)
+        instance.users = validated_data.get('users', instance.users)
+        return instance
+    
+class TodoSerializer(serializers.HyperlinkedModelSerializer):
+ #   note_project = ProjectSerializer()
+ #   user = ListUserSerializer
+    class Meta:
+       model = Todo
+       fields = '__all__'
