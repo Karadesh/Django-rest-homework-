eaa4d9d853556e053b6c7e4654be280e1c6f9e82
Karadesh <akamalavr@gmail.com>
2021-10-08 03:31:06 +0300

    homework 7 is done

--- Todo_project/frontend/src/App.js
+++ Todo_project/frontend/src/App.js
@@ -7,8 +7,15 @@ import FooterList from './components/Footer';
 import TodoList from './components/Todo';
 import ProjectList from './components/Project';
 import {HashRouter, Route, Link} from 'react-router-dom'
+import LoginForm from './components/LoginForm';
 
-
+const NotFound404 = ({location}) => {
+    return(
+        <div>
+            <h1>page on adress '{location.pathname}' is not found</h1>
+        </div>
+    )
+}
 
 class App extends React.Component {
    constructor(props) {
@@ -18,74 +25,123 @@ class App extends React.Component {
            'footers':[],
            'menus': [],
            'project': [],
-           'todos': []
+           'todos': [],
+           'token': ''
        }
    }
 
+   getToken(login, password){
+    axios.post('http://127.0.0.1:8000/api/api-token-auth/', {'username': login, 'password': password})
+    .then(response => {
+        localStorage.setItem('token', response.data)
+        console.log(response.data.token)
+        this.setState({'token': response.data.token}, this.LoadData)
+        
+    })
+    .catch(error => alert('Incorrect password'))
+   } 
+
+   logout(){
+    localStorage.setItem('token','')
+    this.setState({'token': ''}, this.LoadData)
+   }
+
+   isAuthenticated(){
+    return !!this.state.token
+   }
+
+   getHeaders(){
+       if (this.isAuthenticated()){
+       return {'Authorisation': 'Token' + this.state.token}
+       }
+       return {}
+    }
+
+   LoadData(){
+    const headers = this.getHeaders()
+    axios.get('http://127.0.0.1:8000/api/ListUsers/', {headers})
+     .then(response => {
+         const users = response.data
+             this.setState(
+             {
+                 'users': users
+             }
+         )
+
+     
+     }).catch(error =>{
+         this.setState({
+             'users':[]
+         })
+        console.log(error)
+    })
+     axios.get('http://127.0.0.1:8000/api/Todo/', {headers})
+     .then(response => {
+         const todos = response.data
+             this.setState(
+             {
+                 'todos': todos
+             }
+         )
+
+     
+     }).catch(error =>{
+        this.setState({
+            'todos':[]
+        })
+       console.log(error)
+   })
+
+     axios.get('http://127.0.0.1:8000/api/Project/', {headers})
+     .then(response => {
+         const projects = response.data
+             this.setState(
+             {
+                 'project': projects
+             }
+         )
+
+     
+     }).catch(error =>{
+        this.setState({
+            'project':[]
+        })
+       console.log(error)
+   })
+
+     const footers = [
+         {
+         'main': <a href ='http://localhost:8000'>'main'</a>,
+         'api': 'api',
+         'info': 'this is my first rest site! Hope you Enjoy!',
+         'contacts':'127.0.0.1, internet, Russia'
+         }
+     ]
+     this.setState(
+         {
+             'footers': footers
+         }
+     )
+     const menus = [
+         {
+         'main': <a href ='http://localhost:8000'>'main'</a>,
+         'api': <a href ='http://127.0.0.1:8000/api/'>'api'</a>
+         }
+     ]
+     this.setState(
+         {
+             'menus': menus
+         }
+     )
+
+   }
 
    componentDidMount() {
-       axios.get('http://127.0.0.1:8000/api/ListUsers/')
-        .then(response => {
-            const users = response.data
-                this.setState(
-                {
-                    'users': users
-                }
-            )
-
-        
-        }).catch(error => console.log(error))
-        axios.get('http://127.0.0.1:8000/api/Todo/')
-        .then(response => {
-            const todos = response.data
-                this.setState(
-                {
-                    'todos': todos
-                }
-            )
-
-        
-        }).catch(error => console.log(error))
-
-        axios.get('http://127.0.0.1:8000/api/Project/')
-        .then(response => {
-            const projects = response.data
-                this.setState(
-                {
-                    'project': projects
-                }
-            )
-
-        
-        }).catch(error => console.log(error))
-
-        const footers = [
-            {
-            'main': <a href ='http://localhost:8000'>'main'</a>,
-            'api': 'api',
-            'info': 'this is my first rest site! Hope you Enjoy!',
-            'contacts':'127.0.0.1, internet, Russia'
-            }
-        ]
-        this.setState(
-            {
-                'footers': footers
-            }
-        )
-        const menus = [
-            {
-            'main': <a href ='http://localhost:8000'>'main'</a>,
-            'api': <a href ='http://127.0.0.1:8000/api/'>'api'</a>
-            }
-        ]
-        this.setState(
-            {
-                'menus': menus
-            }
-        )
-   }
-   
-        
+        const token = localStorage.getItem('token')
+        console.log(token)
+        this.setState({'token': token}, this.LoadData)
+       
+   }  
     
    
    render () {
@@ -97,13 +153,21 @@ class App extends React.Component {
                 <HashRouter>
                     <nav>
                         <ul>
+
                             <li><Link to='/'>Users</Link></li>
                             <li><Link to='/todo'>Todo</Link></li>
                             <li><Link to='/project'>Project</Link></li>
+                            <li>
+                                {this.isAuthenticated()?
+                                <button onClick={()=>this.logout()}>logout</button>:
+                                <Link to='/login'>login</Link>
+                                }
+                            </li>
                         </ul>
                     </nav> 
                     <Route path='/' exact component={()=><UserList users={this.state.users}/>}/>     
                     <Route path='/todo' exact component={()=><TodoList todovars={this.state.todos}/>}/>  
+                    <Route path='/login' exact component={()=><LoginForm getToken={(login, password)=> this.getToken(login, password)}/>}/>  
                     <Route path='/project' exact component={()=><ProjectList items={this.state.project}/>}/>
                 </HashRouter>
                 <div>
